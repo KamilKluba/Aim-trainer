70c4f00b63f2e87294a96f675ccacb31af5fee52
KamilKluba <226016@student.pwr.edu.pl>
2019-03-18 02:48:06 +0100

    First commit, nothing really happened

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,24 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String args[]){
+        launch(args);
+    }
+
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/Window.fxml"));
+        BorderPane borderPane = loader.load();
+
+        Scene scene = new Scene(borderPane);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
