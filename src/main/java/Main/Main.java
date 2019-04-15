c14c510f171c85dfa670808b8d8a9eddd459c25d
KamilKluba <226016@student.pwr.edu.pl>
2019-04-15 03:33:15 +0200

    Added some things. Nothing big.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,30 +1,47 @@
 package Main;
 
+import controllers.WindowController;
 import javafx.application.Application;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Main extends Application{
+    private double width = 1024;
+    private double height = 576;
+
     public static void main(String args[]){
         launch(args);
     }
 
 
     public void start(Stage primaryStage) throws Exception {
+       // Locale.setDefault(new Locale("en"));
         ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Window.fxml"));
         loader.setResources(resourceBundle);
         TabPane tabPane = loader.load();
 
+        WindowController controller = loader.getController();
+
         Scene scene = new Scene(tabPane);
 
         primaryStage.setMinWidth(1024);
         primaryStage.setMinHeight(576);
+        primaryStage.widthProperty().addListener((observable, oldValue, newValue) -> {
+            width = (double)newValue;
+            controller.resizeButtons(width, height);
+        });
+        primaryStage.heightProperty().addListener((observable, oldValue, newValue) -> {
+            height = (double)newValue;
+            controller.resizeButtons(width, height);
+        });
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/main/java/controllers/WindowController.java
+++ src/main/java/controllers/WindowController.java
@@ -1,15 +1,138 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+import java.util.ArrayList;
 
 public class WindowController {
 
+    private float defaultWidth = 1024;
+    private float defaultHeight = 576;
+    private ArrayList<Button> arrayListButtons = new ArrayList<>();
+    private ArrayList<HBox> arrayListHBoxes = new ArrayList<>();
+    private ArrayList<VBox> arrayListVBoxes = new ArrayList<>();
+
     @FXML
-    Tab tabReflex;
-    Tab tabPrecision;
-    Tab tabSpeed;
+    private Tab tabReflex;
+    @FXML
+    private Tab tabPrecision;
+    @FXML
+    private Tab tabSpeed;
+    @FXML
+    private Button buttonReflex1;
+    @FXML
+    private Button buttonReflex2;
+    @FXML
+    private Button buttonReflex3;
+    @FXML
+    private Button buttonReflex4;
+    @FXML
+    private Button buttonReflex5;
+    @FXML
+    private Button buttonReflex6;
+    @FXML
+    private Button buttonReflex7;
+    @FXML
+    private Button buttonReflex8;
+    @FXML
+    private HBox hBoxReflex1;
+    @FXML
+    private HBox hBoxReflex2;
+    @FXML
+    private VBox vBoxReflex;
+    @FXML
+    private Button buttonPrecision1;
+    @FXML
+    private Button buttonPrecision2;
+    @FXML
+    private Button buttonPrecision3;
+    @FXML
+    private Button buttonPrecision4;
+    @FXML
+    private Button buttonPrecision5;
+    @FXML
+    private Button buttonPrecision6;
+    @FXML
+    private Button buttonPrecision7;
+    @FXML
+    private Button buttonPrecision8;
+    @FXML
+    private HBox hBoxPrecision1;
+    @FXML
+    private HBox hBoxPrecision2;
+    @FXML
+    private VBox vBoxPrecision;
+    @FXML
+    private Button buttonSpeed1;
+    @FXML
+    private Button buttonSpeed2;
+    @FXML
+    private Button buttonSpeed3;
+    @FXML
+    private Button buttonSpeed4;
+    @FXML
+    private Button buttonSpeed5;
+    @FXML
+    private Button buttonSpeed6;
+    @FXML
+    private Button buttonSpeed7;
+    @FXML
+    private Button buttonSpeed8;
+    @FXML
+    private HBox hBoxSpeed1;
+    @FXML
+    private HBox hBoxSpeed2;
+    @FXML
+    private VBox vBoxSpeed;
+
 
     public void initialize(){
+        arrayListButtons.add(buttonReflex1);
+        arrayListButtons.add(buttonReflex2);
+        arrayListButtons.add(buttonReflex3);
+        arrayListButtons.add(buttonReflex4);
+        arrayListButtons.add(buttonReflex5);
+        arrayListButtons.add(buttonReflex6);
+        arrayListButtons.add(buttonReflex7);
+        arrayListButtons.add(buttonReflex8);
+        arrayListHBoxes.add(hBoxReflex1);
+        arrayListHBoxes.add(hBoxReflex2);
+        arrayListVBoxes.add(vBoxReflex);
+
+        arrayListButtons.add(buttonPrecision1);
+        arrayListButtons.add(buttonPrecision2);
+        arrayListButtons.add(buttonPrecision3);
+        arrayListButtons.add(buttonPrecision4);
+        arrayListButtons.add(buttonPrecision5);
+        arrayListButtons.add(buttonPrecision6);
+        arrayListButtons.add(buttonPrecision7);
+        arrayListButtons.add(buttonPrecision8);
+        arrayListHBoxes.add(hBoxPrecision1);
+        arrayListHBoxes.add(hBoxPrecision2);
+        arrayListVBoxes.add(vBoxPrecision);
+
+        arrayListButtons.add(buttonSpeed1);
+        arrayListButtons.add(buttonSpeed2);
+        arrayListButtons.add(buttonSpeed3);
+        arrayListButtons.add(buttonSpeed4);
+        arrayListButtons.add(buttonSpeed5);
+        arrayListButtons.add(buttonSpeed6);
+        arrayListButtons.add(buttonSpeed7);
+        arrayListButtons.add(buttonSpeed8);
+        arrayListHBoxes.add(hBoxSpeed1);
+        arrayListHBoxes.add(hBoxSpeed2);
+        arrayListVBoxes.add(vBoxSpeed);
+    }
+
+    public void resizeButtons(double windowWidth, double windowHeight) {
+        for(Button b : arrayListButtons) b.setPrefSize(windowWidth / defaultWidth * 200,  windowHeight / defaultHeight * 100);
+        buttonReflex1.setFont(new Font("Bank Gothic Medium BT", 5 + (windowHeight + windowWidth) / 150));
+
+        for(HBox h : arrayListHBoxes)h.setSpacing((windowWidth - defaultWidth) / 21 + 30);
     }
 }
